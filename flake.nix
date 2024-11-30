7207a45e20eea55c06cd4b67202a442a0395cf8e
BorisVictoria <boris_victoria@dlsu.edu.ph>
2024-11-30 13:26:57 +0800

    chore: add flake dev environment

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Nix flake for STADVDB";
+
+  # Change this to small if packages are not updated in the unstable branch!
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [ nasm gcc gdb ];
+
+        shellHook = ''
+          nu
+        '';
+
+      };
+    };
+
+}
